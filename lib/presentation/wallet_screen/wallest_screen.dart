c208fa98326b8eaf862fe5616825d3c3a7a61ea4
finosemajeed <finosefinu@gmail.com>
2023-02-14 14:32:26 +0530

    wallet screen completed

--- lib/presentation/wallet_screen/wallest_screen.dart
+++ lib/presentation/wallet_screen/wallest_screen.dart
@@ -1,11 +1,44 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:enabler/domain/core/color_config.dart';
+import 'package:flutter/material.dart';
 
 class WalletScreen extends StatelessWidget {
   const WalletScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroudColor,
+        title: const Text('Wallet'),
+      ),
+      body: Center(
+          child: Wrap(
+        direction: Axis.vertical,
+        crossAxisAlignment: WrapCrossAlignment.center,
+        children: const [
+          Text(
+            'Hey!',
+            style: TextStyle(
+              fontSize: 30,
+              color: kBlack,
+            ),
+          ),
+          Text(
+            'We are cooking it up..',
+            style: TextStyle(
+              fontSize: 30,
+              color: kBlack,
+            ),
+          ),
+          Text(
+            'Stay Tuned!',
+            style: TextStyle(
+              fontSize: 30,
+              color: kYellow,
+            ),
+          ),
+        ],
+      )),
+    );
   }
 }
